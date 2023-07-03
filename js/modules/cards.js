ccca1258003a703c75dd1dfa238ba6a51f85d1ae
sergei <ravinski.sergei@gmail.com>
2023-07-03 13:57:04 +0200

    Created cards array

---
+++ js/modules/cards.js
@@ -0,0 +1,26 @@
+export const cards = [
+	{
+		name: 'boat_1',
+		image: '../../assets/images/boat_1.jpg'
+	},
+	{
+		name: 'boat_2',
+		image: '../../assets/images/boat_2.jpg'
+	},
+	{
+		name: 'boat_3',
+		image: '../../assets/images/boat_3.jpg'
+	},
+	{
+		name: 'boat_4',
+		image: '../../assets/images/boat_4.jpg'
+	},
+	{
+		name: 'boat_5',
+		image: '../../assets/images/boat_5.jpg'
+	},
+	{
+		name: 'boat_6',
+		image: '../../assets/images/boat_6.jpg'
+	},
+]
