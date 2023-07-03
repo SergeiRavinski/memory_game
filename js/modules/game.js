fbd702b6b82484a0da9d45ab9dff2e5023a1f665
sergei <ravinski.sergei@gmail.com>
2023-07-03 14:06:33 +0200

    Created handleCardClick function

--- js/modules/game.js
+++ js/modules/game.js
@@ -50,6 +50,25 @@ export default function Game() {
 
 
 
+
+	function handleCardClick(event) {
+		const currentCard = event.currentTarget;
+		const cardId = currentCard.childNodes[1].id;
+		
+		if (cardsChosen.length < 2 && cardsChosenIds.length < 2 && lockedBoard === false) {
+			lockedBoard = true;
+			cardsChosen.push(currentCard);
+			cardsChosenIds.push(cardId);
+			flipCard(currentCard);
+			lockedBoard = false;
+		}
+		
+		if (cardsChosen.length === 2) {
+			checkMatch(cardsChosenIds);
+		}
+
+		countAttempts();
+	}
 	function countAttempts() {
 		const flattenedArrayOfFlipedCards = cardsFliped.flat();
 
