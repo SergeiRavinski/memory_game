12e6ffeed29ff77e3c5edf5fc0d6e90786ecffc9
sergei <ravinski.sergei@gmail.com>
2023-07-03 13:58:26 +0200

    Created variables

--- js/modules/game.js
+++ js/modules/game.js
@@ -1,5 +1,17 @@
 import { cards } from './cards.js';
 
 export default function Game() {
+	let attempts = 0;
+	let cardsChosen = [];
+	let cardsChosenIds = [];
+	let cardsFliped = [];
+	let lockedBoard = false;
+	
+	const maximumAttempts = 25;
+	const fieldAttempts = document.querySelector('.main__attempts');
+	const board = document.querySelector('.main__cards');
+	const announcement = document.querySelector('.main__announcement');
+	const resetButton = document.querySelector('.main__reset');
+	const duplicatedCards = [...cards, ...cards];
 
 }
