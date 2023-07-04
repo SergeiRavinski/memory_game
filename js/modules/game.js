d20ac0e214ab2dc5e16385f70bda23545d38336c
sergei <ravinski.sergei@gmail.com>
2023-07-04 13:38:37 +0200

    Changed variable

--- js/modules/game.js
+++ js/modules/game.js
@@ -1,13 +1,7 @@
 import { cards } from './cards.js';
 
 export default function Game() {
-	let attempts = 0;
-	let cardsChosen = [];
-	let cardsChosenIds = [];
-	let cardsFliped = [];
-	let lockedBoard = false;
-	
-	const maximumAttempts = 25;
+	const maximumAttempts = 10;
 	const fieldAttempts = document.querySelector('.main__attempts');
 	const board = document.querySelector('.main__cards');
 	const announcement = document.querySelector('.main__announcement');
